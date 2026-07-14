e06c6a4f7492f9e12b85657fc3dfddcc88a23720
Schiggy <hillbrecht.s@gmail.com>
2026-07-14 13:58:28 +0200

    added grid obstacles

--- character/character_state_move.gd
+++ character/character_state_move.gd
@@ -1,19 +1,25 @@
 extends CharacterState
 
+@export var idle_state : CharacterState
 var path : Array[Vector2]
 var line : Line2D
-@export var idle_state : CharacterState
+var is_moving := false
 
 func enter():
 	super()
-	_add_line()
 	step()
 
 func exit():
 	super()
 	line.queue_free()
 
+func update_path(new_path : Array[Vector2]):
+	path = new_path.duplicate()
+	_add_line()
+
 func _add_line():
+	if line != null:
+		line.queue_free()
 	line = Line2D.new()
 	line.points = path
 	line.global_position = Grid._cell_size / 2
@@ -21,8 +27,12 @@ func _add_line():
 	Utils.paths.add_child(line)
 
 func step() -> void:
+	if is_moving: return
+	is_moving = true
 	while not path.is_empty():
 		var next_grid_position = path.pop_front()
+		if next_grid_position == character.global_position:
+			continue
 		var move_tween = create_tween()
 		move_tween.tween_property(
 			character,
@@ -31,4 +41,5 @@ func step() -> void:
 			character.move_duration
 		)
 		await move_tween.finished
+	is_moving = false
 	state_machine.change_state(idle_state)
--- world/tile_map.gd
+++ world/tile_map.gd
@@ -4,6 +4,13 @@ var cell_size = Vector2(16,16)
 var rect : Rect2i
 
 func _ready() -> void:
+	_setup_grid()
+	_add_obstacles_to_grid()
+
+func _setup_grid():
 	rect = get_used_rect()
 	cell_size = Vector2(tile_set.tile_size) * scale
 	Grid.setup(rect, cell_size, global_position)
+
+func _add_obstacles_to_grid():
+	Grid.set_cells_blocked(get_used_cells())
