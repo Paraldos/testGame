fbb5b14c650d21fe426135909a6b03983f687479
Schiggy <hillbrecht.s@gmail.com>
2026-07-15 17:01:46 +0200

    added selector

--- world/unit_selector.gd
+++ world/unit_selector.gd
@@ -2,11 +2,33 @@ extends Area2D
 
 @onready var selection_shape: CollisionShape2D = %SelectionShape
 @onready var selection_visual: Line2D = %SelectionVisual
-var start_point
+var _start_point : Vector2
+var _end_point : Vector2
+var is_selecting = false
 
 func _ready() -> void:
 	monitoring = false
 	selection_visual.visible = false
+
+func _process(_delta: float) -> void:
+	if is_selecting: _draw_selector()
+
+func _draw_selector():
+	_end_point = get_global_mouse_position()
+	var size := (_end_point - _start_point).abs()
+	var center := (_start_point + _end_point) / 2.0
+	var rectangle_shape := selection_shape.shape as RectangleShape2D
+	var half_size := size / 2.0
+	selection_shape.global_position = center
+	rectangle_shape.size = size
+	selection_visual.position = center
+	selection_visual.points = PackedVector2Array([
+		Vector2(-half_size.x, -half_size.y),
+		Vector2( half_size.x, -half_size.y),
+		Vector2( half_size.x,  half_size.y),
+		Vector2(-half_size.x,  half_size.y),
+		Vector2(-half_size.x, -half_size.y)
+	])
 
 func _unhandled_input(event: InputEvent) -> void:
 	_left_click(event)
@@ -15,4 +37,10 @@ func _left_click(event: InputEvent):
 	if event is not InputEventMouseButton: return
 	if event.button_index != MOUSE_BUTTON_LEFT: return
 	if event.pressed:
-		pass
+		_start_point = get_global_mouse_position()
+		_end_point = _start_point
+		is_selecting = true
+		selection_visual.visible = true
+	if event.is_released():
+		is_selecting = false
+		selection_visual.visible = false
